b6e014067ef58ebb68347a3a4e5502b05619fa4c
Demetri <demetriv.s.7@gmail.com>
2021-09-04 13:53:14 -0700

    Added code for checking and storing station occupancy

--- TransitTracker/TransitTracker.ino
+++ TransitTracker/TransitTracker.ino
@@ -10,15 +10,21 @@ struct trains
   uint8_t numOfTrains;
   double lat[49];
   double lon[49];
-}redLine,blueLine,greenLine,yellowLine,orangeLine;
+}redLineTrains,blueLineTrains,greenLineTrains,yellowLineTrains,orangeLineTrains;
 
-#include "functions.h"
+const uint16_t RED_ROUTE_ID = 90;
+const uint16_t BLUE_ROUTE_ID = 100;
+const uint16_t GREEN_ROUTE_ID = 200;
+const uint16_t YELLOW_ROUTE_ID = 190;
+const uint16_t ORANGE_ROUTE_ID = 290;
 
 const uint8_t NUM_RED_STOPS = 27;
 const uint8_t NUM_BLUE_STOPS = 49;
 const uint8_t NUM_GREEN_STOPS = 30;
 const uint8_t NUM_YELLOW_STOPS = 17;
 const uint8_t NUM_ORANGE_STOPS = 17;
+
+bool canReadOccupancy = true;
 
 
 struct redStationBounds
@@ -27,7 +33,7 @@ struct redStationBounds
                                       45.572240,45.577100,45.587450};
   const double LON[NUM_RED_STOPS] = {-122.801489,-122.781002,-122.716818,-122.693753,-122.689808,-122.681582,-122.679153,-122.675068,-122.689874,-122.681937,-122.679445,-122.674467,-122.672304,-122.671355,-122.671404,-122.665214,-122.662237,-122.658060,-122.654048,-122.622923,
                                       -122.602810,-122.578208,-122.563680,-122.565560,-122.558030,-122.567150,-122.592980};
-  const double LAT_DIFF_MIN[NUM_RED_STOPS] = {0.000295,0.000196,0.000276,0.000203,0.000125,0.000125,0.000200,0.000200,0.000125,0.000125,0.000200,0.000200,0.000350,0.000550,0.000550,0.000200,0.000200,0.000200,0.000200,0.000355,0.000455,0.000670,0.000600,0.000430,0.000290,0.000380,0.000320};
+  const double LAT_DIFF_MAX[NUM_RED_STOPS] = {0.000295,0.000196,0.000276,0.000203,0.000125,0.000125,0.000200,0.000200,0.000125,0.000125,0.000200,0.000200,0.000350,0.000550,0.000550,0.000200,0.000200,0.000200,0.000200,0.000355,0.000455,0.000670,0.000600,0.000430,0.000290,0.000380,0.000320};
   const double LON_DIFF_MAX[NUM_RED_STOPS] = {0.000316,0.000648,0.000713,0.000795,0.000470,0.000470,0.000470,0.000600,0.000470,0.000470,0.000600,0.000600,0.000300,0.000200,0.000200,0.000648,0.000520,0.000520,0.000570,0.002879,0.000900,0.000900,0.000400,0.000590,0.000530,0.000330,0.000440};
 }RED_STATION_BOUNDS;
 
@@ -39,7 +45,7 @@ struct blueStationBounds
   const double LON[NUM_BLUE_STOPS] = {-122.991065,-122.985280,-122.978333,-122.969505,-122.946069,-122.930690,-122.915874,-122.888806,-122.870175,-122.851473,-122.842209,-122.833050,-122.821574,-122.807056,-122.801489,-122.781002,-122.716818,-122.693753,-122.689808,
                                         -122.689874,-122.679153,-122.675068,-122.674467,-122.679445,-122.681937,-122.689874,-122.672304,-122.671355,-122.671404,-122.665214,-122.662237,-122.658060,-122.654048,-122.622923,-122.602810,-122.578208,-122.563680,-122.55894,
                                           -122.538440,-122.511330,-122.496469,-122.486156,-122.477125,-122.470798,-122.460323,-122.441589,-122.436077,-122.427054,-122.418355};
-  const double LAT_DIFF_MIN[NUM_BLUE_STOPS] = {0.000380,0.000200,0.000150,0.000300,0.000250,0.000250,0.000250,0.000250,0.000250,0.000280,0.000300,0.000368,0.000236,0.000250,0.000295,0.000196,0.000276,0.000203,0.000125,0.000125,0.000200,0.000200,0.000200,0.000200,0.000125,
+  const double LAT_DIFF_MAX[NUM_BLUE_STOPS] = {0.000380,0.000200,0.000150,0.000300,0.000250,0.000250,0.000250,0.000250,0.000250,0.000280,0.000300,0.000368,0.000236,0.000250,0.000295,0.000196,0.000276,0.000203,0.000125,0.000125,0.000200,0.000200,0.000200,0.000200,0.000125,
                                                 0.000125,0.000350,0.000550,0.000550,0.000200,0.000200,0.000200,0.000200,0.000355,0.000455,0.000670,0.000600,0.000125,0.000125,0.000125,0.000125,0.000125,0.000125,0.000280,0.000590,0.000220,0.000240,0.000245,0.000245};
   const double LON_DIFF_MAX[NUM_BLUE_STOPS] = {0.000260,0.000540,0.000560,0.009400,0.000630,0.000530,0.000500,0.000535,0.000550,0.000480,0.000510,0.000650,0.000530,0.000530,0.000316,0.000648,0.000713,0.000795,0.000470,0.000470,0.000470,0.000600,0.000600,0.000600,0.000470,
                                                 0.000470,0.000300,0.000200,0.000200,0.000648,0.000520,0.000520,0.000570,0.002879,0.000900,0.000900,0.000400,0.000610,0.000428,0.000550,0.001150,0.001180,0.001200,0.000475,0.001215,0.000425,0.000495,0.000500,0.000735};
@@ -51,7 +57,7 @@ struct greenStationBounds
                                         45.503392,45.495736,45.491097,45.480500,45.467898,45.453426,45.435763};
   const double LON[NUM_GREEN_STOPS] = {-122.683692,-122.682885,-122.682700,-122.681400,-122.680460,-122.679630,-122.678722,-122.677779,-122.67690,-122.676042,-122.676409,-122.675499,-122.676517,-122.675637,-122.665214,-122.662237,-122.658060,-122.654048,-122.622923,
                                         -122.602810,-122.578208,-122.563680,-122.564438,-122.566631,-122.566207,-122.566270,-122.566810,-122.566617,-122.573612,-122.567768};
-  const double LAT_DIFF_MIN[NUM_GREEN_STOPS] = {0.000430,0.000340,0.000320,0.000450,0.000395,0.000370,0.000420,0.000390,0.000390,0.000480,0.000420,0.000345,0.000290,0.000340,0.000200,0.000200,0.000200,0.000200,0.000355,0.000455,0.000670,0.000600,0.000670,0.000400,
+  const double LAT_DIFF_MAX[NUM_GREEN_STOPS] = {0.000430,0.000340,0.000320,0.000450,0.000395,0.000370,0.000420,0.000390,0.000390,0.000480,0.000420,0.000345,0.000290,0.000340,0.000200,0.000200,0.000200,0.000200,0.000355,0.000455,0.000670,0.000600,0.000670,0.000400,
                                                   0.000400,0.000440,0.000970,0.000469,0.000470,0.000365};
   const double LON_DIFF_MAX[NUM_GREEN_STOPS] = {0.000250,0.000300,0.000270,0.000270,0.000350,0.000300,0.000390,0.000330,0.000300,0.000370,0.000370,0.000290,0.000250,0.000250,0.000648,0.000520,0.000520,0.000570,0.002879,0.000900,0.000900,0.000400,0.000250,0.000250,
                                                   0.000250,0.000250,0.000250,0.000375,0.000375,0.000260};
@@ -61,7 +67,7 @@ struct yellowStationBounds
 {
   const double LAT[NUM_YELLOW_STOPS] = {45.509471,45.511890,45.515480,45.518783,45.522120,45.524251,45.527024,45.530170,45.539558,45.548708,45.555180,45.562655,45.569854,45.577117,45.583850,45.596135,45.605440};
   const double LON[NUM_YELLOW_STOPS] = {-122.683692,-122.681400,-122.680460,-122.678722,-122.676900,-122.676409,-122.676517,-122.667810,-122.675524,-122.680996,-122.682312,-122.682244,-122.682139,-122.682115,-122.685931,-122.685660,-122.685542};
-  const double LAT_DIFF_MIN[NUM_YELLOW_STOPS] = {0.000430,0.000450,0.000395,0.000420,0.000390,0.000420,0.000290,0.000289,0.000289,0.000850,0.000300,0.000715,0.000815,0.000785,0.000250,0.000335,0.000355};
+  const double LAT_DIFF_MAX[NUM_YELLOW_STOPS] = {0.000430,0.000450,0.000395,0.000420,0.000390,0.000420,0.000290,0.000289,0.000289,0.000850,0.000300,0.000715,0.000815,0.000785,0.000250,0.000335,0.000355};
   const double LON_DIFF_MAX[NUM_YELLOW_STOPS] = {0.000250,0.000270,0.000350,0.000390,0.000300,0.000370,0.000250,0.000550,0.000420,0.000250,0.000150,0.000150,0.000200,0.000150,0.000450,0.000200,0.000375};
 }YELLOW_STATION_BOUNDS;
 
@@ -69,12 +75,21 @@ struct orangeStationBounds
 {
   const double LAT[NUM_ORANGE_STOPS] = {45.430771,45.441215,45.462789,45.474817,45.490902,45.497904,45.502779,45.506565,45.502757,45.507911,45.509183,45.511890,45.515130,45.518492,45.521749,45.524147,45.527026};
   const double LON[NUM_ORANGE_STOPS] = {-122.635129,-122.639947,-122.637634,-122.640033,-122.648508,-122.64817,-122.653267,-122.662755,-122.671252,-122.680519,-122.682885,-122.681400,-122.679630,-122.677779,-122.676042,-122.675499,-122.675637};
-  const double LAT_DIFF_MIN[NUM_ORANGE_STOPS] = {0.000368,0.000375,0.000375,0.000570,0.000400,0.000400,0.000320,0.000150,0.000340,0.000150,0.000340,0.000450,0.000370,0.000390,0.000480,0.000345,0.000340};
+  const double LAT_DIFF_MAX[NUM_ORANGE_STOPS] = {0.000368,0.000375,0.000375,0.000570,0.000400,0.000400,0.000320,0.000150,0.000340,0.000150,0.000340,0.000450,0.000370,0.000390,0.000480,0.000345,0.000340};
   const double LON_DIFF_MAX[NUM_ORANGE_STOPS] = {0.000250,0.000300,0.000540,0.000256,0.000150,0.000150,0.000500,0.000675,0.000770,0.000515,0.000300,0.000270,0.000300,0.000330,0.000370,0.000290,0.000250};
 }ORANGE_STATION_BOUNDS;
 
+struct stationOccupancy
+{
+  bool redStation[NUM_RED_STOPS] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+  bool blueStation[NUM_BLUE_STOPS] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,
+                                              false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+  bool greenStation[NUM_GREEN_STOPS] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+  bool yellowStation[NUM_YELLOW_STOPS] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+  bool orangeStation[NUM_ORANGE_STOPS] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
+}stationFilledLive;
 
-
+#include "functions.h"
 
 Adafruit_NeoPixel ledsRedLine(NUM_RED_STOPS, 14, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel ledsBlueLine(NUM_BLUE_STOPS, 12, NEO_GRB + NEO_KHZ800);
@@ -112,64 +127,43 @@ void setup() {
 }
 
 void loop() {
+  
+    String apiCallResult; 
 
-    Serial.print("connecting to ");
-    Serial.println("developer.trimet.org");
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-    const int httpPort = 80;
-    if (!client.connect("developer.trimet.org", httpPort)) {
-        Serial.println("connection failed");
-        return;
-    }
-
-    // We now create a URI for the request
-    String url = "/ws/V2/vehicles/onRouteOnly/true/showStale/false/xml/true/routes/100/appID/";
-    url += API_KEY;
+    callTriMetApi(RED_ROUTE_ID, &apiCallResult);
     
-    Serial.print("Requesting URL: ");
-    Serial.println(url);
+    Serial.print(apiCallResult);
 
-    // This will send the request to the server
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + "developer.trimet.org" + "\r\n" +
-                 "Connection: close\r\n\r\n");
-    unsigned long timeout = millis();
-    while (client.available() == 0) {
-        if (millis() - timeout > 5000) {
-            Serial.println(">>> Client Timeout !");
-            client.stop();
-            return;
-        }
-    }
-
-
-    String line; 
-
-    line = client.readString();
+    parseApiString(&apiCallResult, &redLineTrains);
     
-    Serial.print(line);
-
-    parseApiString(&line, &blueLine);
-    
-    Serial.print(line);
     Serial.println();
-    Serial.println("closing connection");
+
 
     Serial.println("--------------");
-    Serial.println(blueLine.numOfTrains);
+    Serial.println(redLineTrains.numOfTrains);
     Serial.println("--------------");
     
-    for(int i = 0; i < 49; i++)
+    for(int i = 0; i < 27; i++)
     {
-      Serial.print(blueLine.lat[i],7);
+      Serial.print(redLineTrains.lat[i],7);
       Serial.print(" , ");
-      Serial.println(blueLine.lon[i],6);
+      Serial.println(redLineTrains.lon[i],6);
     }
 
-    
+    findStationOccupancy();
 
-    while(true){}
+    for(int i = 0; i < 27; i++)
+    {
+      if(stationFilledLive.redStation[i] == true)
+      {
+        Serial.print("Train at: ");
+        Serial.print(RED_STATION_BOUNDS.LAT[i],7);
+        Serial.print(" , ");
+        Serial.println(RED_STATION_BOUNDS.LON[i],6);
+        Serial.println(i);
+      }
+    }
+while(true){}
+   //delay(55000);
 
 }
--- TransitTracker/functions.h
+++ TransitTracker/functions.h
@@ -66,7 +66,7 @@ void initTrainStruct(struct trains *trainLine)
  *  
  * Arguments:
  *  (IN) apiString -- pointer to string from api call
- *  (IN/OUT) trainLine -- pointer to trains stuct
+ *  (OUT) trainLine -- pointer to trains stuct
  *  
  * Returns:
  *  void
@@ -109,4 +109,233 @@ void parseApiString(String * apiString, struct trains * trainLine)
     numOfTrains++;
   }
   trainLine->numOfTrains = numOfTrains;
+}
+
+
+/*
+ * Description: Makes a call to the API and stores that data in a string
+ * 
+ * Arguments:
+ *  (IN) routeID -- route number that is being called
+ *  (OUT) callResult -- pointer to string that will hold the call result
+ *  
+ * Return:
+ *  false -- call failed
+ *  true -- call went through fine
+ */
+bool callTriMetApi(const uint16_t ROUTE_ID, String * callResult)
+{
+  Serial.print("connecting to ");
+  Serial.println("developer.trimet.org");
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect("developer.trimet.org", httpPort)) {
+      Serial.println("connection failed");
+      return false;
+  }
+
+  // We now create a URI for the request
+  String url = "/ws/V2/vehicles/onRouteOnly/true/showStale/false/xml/true/routes/";
+  url += ROUTE_ID;
+  url += "/appID/";
+  url += API_KEY;
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + "developer.trimet.org" + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while(client.available() == 0) 
+  {
+    if(millis() - timeout > 5000) 
+    {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return false;
+    }
+  }
+
+  *callResult = client.readString();
+  Serial.println("closing connection");
+  return true;
+}
+
+
+
+/*
+ * Description: Calcuates what stations have trains at them
+ * 
+ * Arguments:
+ *  none
+ *  
+ * Returns:
+ *  void
+ */
+void findStationOccupancy()
+{
+  double latDiff = 0;
+  double lonDiff = 0;
+
+  struct stationOccupancy stationFilledOffline;
+
+  //Reset the offline copy of the station occupancy struct
+  for(uint8_t indx = 0; indx < 49; indx++)
+  {
+    if(indx < NUM_RED_STOPS)
+    {
+      stationFilledOffline.redStation[indx] = false;
+    }
+    
+    if(indx < NUM_BLUE_STOPS)
+    {
+      stationFilledOffline.blueStation[indx] = false;
+    }
+    
+    if(indx < NUM_GREEN_STOPS)
+    {
+      stationFilledOffline.greenStation[indx] = false;
+    }
+
+    if(indx < NUM_YELLOW_STOPS)
+    {
+      stationFilledOffline.yellowStation[indx] = false;
+    }
+
+    if(indx < NUM_ORANGE_STOPS)
+    {
+      stationFilledOffline.orangeStation[indx] = false;
+    }
+  }
+  
+  //Find occupancy for red line stations
+  for(uint8_t trainIndx = 0; trainIndx < redLineTrains.numOfTrains; trainIndx++)
+  {
+    for(uint8_t stopIndx = 0; stopIndx < NUM_RED_STOPS; stopIndx++)
+    {
+      latDiff = RED_STATION_BOUNDS.LAT[stopIndx] - redLineTrains.lat[trainIndx];
+      latDiff = abs(latDiff);
+      
+      lonDiff = redLineTrains.lon[trainIndx] - RED_STATION_BOUNDS.LON[stopIndx];
+      lonDiff = abs(lonDiff);
+      
+      if(latDiff <= RED_STATION_BOUNDS.LAT_DIFF_MAX[stopIndx] && lonDiff <= RED_STATION_BOUNDS.LON_DIFF_MAX[stopIndx])
+      {
+        stationFilledOffline.redStation[stopIndx] = true;
+        break;
+      }
+    }
+  }
+
+  //Find occupancy for blue line stations
+  for(uint8_t trainIndx = 0; trainIndx < blueLineTrains.numOfTrains; trainIndx++)
+  {
+    for(uint8_t stopIndx = 0; stopIndx < NUM_BLUE_STOPS; stopIndx++)
+    {
+      latDiff = BLUE_STATION_BOUNDS.LAT[stopIndx] - blueLineTrains.lat[trainIndx];
+      latDiff = abs(latDiff);
+      
+      lonDiff = blueLineTrains.lon[trainIndx] - BLUE_STATION_BOUNDS.LON[stopIndx];
+      lonDiff = abs(lonDiff);
+      
+      if(latDiff <= BLUE_STATION_BOUNDS.LAT_DIFF_MAX[stopIndx] && lonDiff <= BLUE_STATION_BOUNDS.LON_DIFF_MAX[stopIndx])
+      {
+        stationFilledOffline.blueStation[stopIndx] = true;
+        break;
+      }
+    }
+  }
+
+  //Find occupancy for green line stations
+  for(uint8_t trainIndx = 0; trainIndx < greenLineTrains.numOfTrains; trainIndx++)
+  {
+    for(uint8_t stopIndx = 0; stopIndx < NUM_GREEN_STOPS; stopIndx++)
+    {
+      latDiff = GREEN_STATION_BOUNDS.LAT[stopIndx] - greenLineTrains.lat[trainIndx];
+      latDiff = abs(latDiff);
+      
+      lonDiff = greenLineTrains.lon[trainIndx] - GREEN_STATION_BOUNDS.LON[stopIndx];
+      lonDiff = abs(lonDiff);
+      
+      if(latDiff <= GREEN_STATION_BOUNDS.LAT_DIFF_MAX[stopIndx] && lonDiff <= GREEN_STATION_BOUNDS.LON_DIFF_MAX[stopIndx])
+      {
+        stationFilledOffline.greenStation[stopIndx] = true;
+        break;
+      }
+    }
+  }
+
+  //Find occupancy for yellow line stations
+  for(uint8_t trainIndx = 0; trainIndx < yellowLineTrains.numOfTrains; trainIndx++)
+  {
+    for(uint8_t stopIndx = 0; stopIndx < NUM_YELLOW_STOPS; stopIndx++)
+    {
+      latDiff = YELLOW_STATION_BOUNDS.LAT[stopIndx] - yellowLineTrains.lat[trainIndx];
+      latDiff = abs(latDiff);
+      
+      lonDiff = yellowLineTrains.lon[trainIndx] - YELLOW_STATION_BOUNDS.LON[stopIndx];
+      lonDiff = abs(lonDiff);
+      
+      if(latDiff <= YELLOW_STATION_BOUNDS.LAT_DIFF_MAX[stopIndx] && lonDiff <= YELLOW_STATION_BOUNDS.LON_DIFF_MAX[stopIndx])
+      {
+        stationFilledOffline.yellowStation[stopIndx] = true;
+        break;
+      }
+    }
+  }
+
+  //Find occupancy for orange line stations
+  for(uint8_t trainIndx = 0; trainIndx < orangeLineTrains.numOfTrains; trainIndx++)
+  {
+    for(uint8_t stopIndx = 0; stopIndx < NUM_ORANGE_STOPS; stopIndx++)
+    {
+      latDiff = ORANGE_STATION_BOUNDS.LAT[stopIndx] - yellowLineTrains.lat[trainIndx];
+      latDiff = abs(latDiff);
+      
+      lonDiff = orangeLineTrains.lon[trainIndx] - ORANGE_STATION_BOUNDS.LON[stopIndx];
+      lonDiff = abs(lonDiff);
+      
+      if(latDiff <= ORANGE_STATION_BOUNDS.LAT_DIFF_MAX[stopIndx] && lonDiff <= ORANGE_STATION_BOUNDS.LON_DIFF_MAX[stopIndx])
+      {
+        stationFilledOffline.orangeStation[stopIndx] = true;
+        break;
+      }
+    }
+  }
+
+  //Update live struct
+  canReadOccupancy = false;
+  for(uint8_t indx = 0; indx < 49; indx++)
+  {
+    if(indx < NUM_RED_STOPS)
+    {
+      stationFilledLive.redStation[indx] = stationFilledOffline.redStation[indx];
+    }
+    
+    if(indx < NUM_BLUE_STOPS)
+    {
+      stationFilledLive.blueStation[indx] = stationFilledOffline.blueStation[indx];
+    }
+    
+    if(indx < NUM_GREEN_STOPS)
+    {
+      stationFilledLive.greenStation[indx] = stationFilledOffline.greenStation[indx];
+    }
+
+    if(indx < NUM_YELLOW_STOPS)
+    {
+      stationFilledLive.yellowStation[indx] = stationFilledOffline.yellowStation[indx];
+    }
+
+    if(indx < NUM_ORANGE_STOPS)
+    {
+      stationFilledLive.orangeStation[indx] = stationFilledOffline.orangeStation[indx];
+    }
+  }
+  canReadOccupancy = true;
+  
 }
